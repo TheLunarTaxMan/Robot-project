e5db0d80f371187d67fb4214ea8f1ffd35e3c66e
TheLunarTaxMan <154992215+TheLunarTaxMan@users.noreply.github.com>
2025-08-04 14:29:49 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,1 +1,19 @@
-#aaaaaaaaaaaaaaaaaaaaaah
+from sbot import *
+
+def Main():
+
+def GetDistance():
+  distance = arduino.measure_ultrasound_distance(,)
+
+def rotateLeft():
+  motors.set_power(0, 0.8)
+  sleep(1)
+  motors.set_power(0,0)
+  
+def rotateRight():
+  motors.set_power(1, 0.8)
+  sleep(1)
+  motors.set_power(1,0)
+
+def WriteStatusToLog():
+  #put some stuff here
