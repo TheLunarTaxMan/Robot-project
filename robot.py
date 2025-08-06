d9d4579f73bfeadc4d0ced96cea58528175bc488
TheLunarTaxMan <154992215+TheLunarTaxMan@users.noreply.github.com>
2025-08-06 17:57:24 +0100

    fixed the code somewhat

--- robot.py
+++ robot.py
@@ -1,4 +1,4 @@
-from sbot import arduino, motors, utils, AnalogPin, vision
+from sbot import arduino, motors, utils, AnalogPin, vision, GPIOPinMode
 import math
 
 
@@ -19,32 +19,33 @@ def set_state(state):
 
 #calculates marker distances
 def markerLocater(markerList):
+    closest = markerList[0]
     for marker in markerList:
         distance = marker.position.distance
-        listOfMarkerDistances.append(distance)
+        if distance > closest.position.distance:
+            closest = marker
+    return closest
 
-while True:
-    left_IR = arduino.analog_read(AnalogPin.A0)
-    centre_IR = arduino.analog_read(AnalogPin.A1)
-    right_IR = arduino.analog_read(AnalogPin.A2)
-    UltraDistance = arduino.measure_ultrasound_distance(2,3)
-    #Stores current QRCodes in List
+
+def markerchasing():
     markerList = vision.detect_markers()
     if len(markerList) > 0:
-        markerLocater(markerList)
+        markerTargeted = markerLocater(markerList)
         #Finds smallest value in List
-        markerTargeted = min(listOfMarkerDistances)
-        if markerTargeted >= 300 and markerTargeted <= 3000:
+        if markerTargeted.location.distance >= 300 and markerTargeted.location.distance <= 3000:
             #Find of a way to identify which marker corresponds to the shortest distance
-            
-            
             turnAngle = markerTargeted.position.horizontal_angle
         else:
-            turnAngle = 0.3
-        listOfMarkerDistances.clear()
-        
+            turnAngle = 0.3 
+        return True       
     else:
         print("nomarkers")
+        return False
+
+def linefollowing():
+    left_IR = arduino.analog_read(AnalogPin.A3)
+    centre_IR = arduino.analog_read(AnalogPin.A4)
+    right_IR = arduino.analog_read(AnalogPin.A5)
     print(left_IR, centre_IR, right_IR)
     if left_IR < 1.2 and centre_IR > 3.5 and right_IR < 1.2:
         current_state = "forward"
@@ -52,5 +53,37 @@ while True:
         current_state = "left"
     if left_IR < 3.5 and centre_IR > 1.2 and right_IR < 1.2:
         current_state = "right"
-    markerList.clear()
+
+def whereami():
+    return 1
+
+arduino.set_pin_mode(AnalogPin.A5, GPIOPinMode.Input)
+arduino.set_pin_mode(AnalogPin.A4, GPIOPinMode.Input)
+arduino.set_pin_mode(AnalogPin.A3, GPIOPinMode.Input)
+
+arduino.set_pin_mode(10, GPIOPinMode.Input)
+arduino.set_pin_mode(11, GPIOPinMode.Input)
+arduino.set_pin_mode(12, GPIOPinMode.Input)
+arduino.set_pin_mode(13, GPIOPinMode.Input)
+
+
+chasing = False
+
+while True:
+    UltraDistance = arduino.measure_ultrasound_distance(11,10)
+    #Stores current QRCodes in List
+    location = whereami()
+    if location == 1 or location == 2:
+        chasing = True
+    elif location == 3 or location == 4:
+        chasing = False
+    else:
+        chasing = False
+    
+    
+
+    if chasing:
+        markerchasing()
+    else:
+        linefollowing()    
     set_state(current_state)
